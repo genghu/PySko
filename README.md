7d17613bb2bb69b226264773796500ed1f458576
Ubuntu <ubuntu@ip-172-31-30-142.ap-northeast-1.compute.internal>
2015-08-26 01:11:34 +0000

    starting over

--- README.md
+++
@@ -1,3 +0,0 @@
-# PySko
-
-SKO Python
--- SpaceList.py
+++
@@ -1,5 +0,0 @@
-import tornado.web
-
-class SpaceList(tornado.web.RequestHandler):
-    def get(self):
-        self.write('<spaces><space value="fa" label="Free Association (by human)"/></spaces>')
